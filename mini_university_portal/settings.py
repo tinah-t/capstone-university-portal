50ad4b40287bc8d7a79d3ee03f9a5af81118a8f4
Hana <gerenett@gmail.com>
2025-12-13 10:43:53 +0300

    created a registration app

--- mini_university_portal/settings.py
+++ mini_university_portal/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'htu_portal',
     'rest_framework',
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
